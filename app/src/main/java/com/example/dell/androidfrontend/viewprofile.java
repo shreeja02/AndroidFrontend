5eae82ca45f8b492ba83fa6de5741b5d94924392
hetshah27 <hetshah7@gmail.com>
2017-05-31 21:39:05 +0530

    view profile

---
+++ app/src/main/java/com/example/dell/androidfrontend/viewprofile.java
@@ -0,0 +1,72 @@
+package com.example.dell.androidfrontend;
+
+/**
+ * Created by DELL on 5/30/2017.
+ */
+
+public class viewprofile {
+
+    String email_id;
+    String user_name;
+    String password;
+    String user_photo;
+    String mobile_no;
+    String gender;
+
+    public viewprofile(String email_id, String user_name, String password, String user_photo, String mobile_no, String gender) {
+        this.email_id = email_id;
+        this.user_name = user_name;
+        this.password = password;
+        this.user_photo = user_photo;
+        this.mobile_no = mobile_no;
+        this.gender = gender;
+    }
+
+    public String getEmail_id() {
+        return email_id;
+    }
+
+    public void setEmail_id(String email_id) {
+        this.email_id = email_id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUser_photo() {
+        return user_photo;
+    }
+
+    public void setUser_photo(String user_photo) {
+        this.user_photo = user_photo;
+    }
+
+    public String getMobile_no() {
+        return mobile_no;
+    }
+
+    public void setMobile_no(String mobile_no) {
+        this.mobile_no = mobile_no;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
